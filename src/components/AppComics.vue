731f18a5033add57fbc7566cda2fdf0bda4e1308
mattia_morriale <mattia.morriale1@gmail.com>
2024-03-01 16:20:33 +0100

    first commit add img

---
+++ src/components/AppComics.vue
@@ -0,0 +1,49 @@
+<script>
+
+    export default {
+
+        name: 'AppComics',
+
+        props: {
+            comicsThumb: String,
+            comicsSeries: String,
+        },
+
+        data() {
+        },
+
+    }
+
+</script>
+
+<template>
+
+    <div class="comics">
+        <img :src="comicsThumb" alt="#">        
+        <h4>{{ comicsSeries }}</h4>
+    </div>
+
+</template>
+
+<style>
+
+.comics{
+    width: calc(100% / 6 - 20px / 6 * 5);
+
+    margin-bottom: 25px;
+    
+
+    img{
+        height: 190px;
+        width: 190px;
+
+        object-fit: cover;
+        object-position: top;
+
+        margin-bottom: 10px;
+    }
+    
+
+}
+
+</style>
